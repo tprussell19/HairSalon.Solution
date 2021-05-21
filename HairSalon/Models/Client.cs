70f58ffa4fba22ded52d97ecfbe1b611393a4ec3
Thomas Russell <t.p.russell19@gmail.com>
2021-05-21 12:54:06 -0700

    set class object properties and build schema and tables

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -6,9 +6,6 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
-    public int Price { get; set; }
-    public string Location { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
   }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -10,6 +10,7 @@ namespace HairSalon.Models
     }
     public int StylistId { get; set; }
     public string Name { get; set; }
+    public string ServiceProvided { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
   }
 }
